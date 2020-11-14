14f3e9909ee6fad09ef91319fb1915d23f1cda1e
Crowdbotics <team@crowdbotics.com>
2020-11-14 15:29:59 +0000

    Update models

--- backend/dating_app_for_lose_22618/wsgi.py
+++ backend/dating_app_for_lose_22618/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dating_app_for_lose_22618.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dating_app_for_lose_22618.settings")
 
 application = get_wsgi_application()
